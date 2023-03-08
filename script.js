e888f296f7d49c17eff7795dc2cb11e5718cdc01
Maria Clara Borges <111436040+mariaclaraborges@users.noreply.github.com>
2023-03-07 21:04:37 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,22 +1,28 @@
-//aprendendo mais sobre a lógica da API
-
-//fetch: fornece uma maneira fácil e lógica para buscar recursos de forma assíncrona através da rede.
-//O uso do fetch() acarreta em um argumento — a pasta do recurso que você deseja buscar — e retorna uma promessa (promise) contendo a resposta (response object).
-
-var consultaCEP = fetch('https://viacep.com.br/ws/01001000/json/') 
-  //método then: lida com os dados recebidos pela requisição. 
-  //o dado recebido foi o objeto do tipo Response que traz um corpo de resposta que não da para acessar
-    .then(resposta => resposta.json())//1º uso: transformar a resposta em JSON
-    .then(r => {
-        if(r.erro){ //se true
-          throw Error('Esse CEP não existe.')
-        } else{
-         console.log(r)
+//Aprendendo async
+ 
+ 
+ async function buscaEndereco(cep){
+    try{
+        var consultaCEP = await fetch (`https://viacep.com.br/ws/${cep}/json/`)
+        var consultaCEPConvertida = await consultaCEP.json()
+        if (consultaCEPConvertida.erro){//caso seja um cep com os dígitos necessários, mas inexistente, manda-se um erro = true
+            throw Error('CEP não existente!')
         }
-        })
-    .catch(erro => console.log(erro))//catch: quando a promessa for rejeitada; "pega o throw e imprime o erro na tela"
-    .finally(mensagem => console.log('Processamento concluído.'))//aparece independente se a promessa foi resolvida ou não.
-    
+        console.log(consultaCEPConvertida);
+        return consultaCEPConvertida
+    }
+    catch(erro){//erro 400 por ex
+        console.log(erro)
+    }
+ 
+ }
 
-    
-//uma requisição é composta de uma request(solicitação) e uma response(resposta)/
+//um CEP:
+ buscaEndereco(12346899)
+
+//mais de um CEP:
+//let ceps = ['01001000', '01001001'];
+//let conjuntosCeps = ceps.map(valores => buscaEndereco(valores));
+//.map vai fazer um array de conjunto de CEP's. como parâmetro, buscaEndereco() receberá os CEPs presentes no array de ceps
+//Promise.all(conjuntosCeps).then(respostas => console.log(respostas))
+//promise all vai resolver o array de promessas, e vamos pedir para imprimir com o then (imprime o que foi resolvido)
