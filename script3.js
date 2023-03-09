375ae565f2632a9cb95c756b0cec651bfe45e923
Maria Clara Borges <mariaclarapiola@hotmail.com>
2023-03-09 09:55:17 -0300

    script3.js

--- script3.js
+++ script3.js
@@ -1,27 +1,32 @@
 //manipulando o forms
- async function buscaEndereco(cep){
+ async function buscaEndereco(cep){;
+
+    //1 var mensagemErro = document.getElementById('erro');
+    //1 mensagemErro.innerHTML = "";
     try{
-        var consultaCEP = await fetch (`https://viacep.com.br/ws/${cep}/json/`)
-        var consultaCEPConvertida = await consultaCEP.json()
+        var consultaCEP = await fetch (`https://viacep.com.br/ws/${cep}/json/`);
+        var consultaCEPConvertida = await consultaCEP.json();
         if (consultaCEPConvertida.erro){//caso seja um cep com os dígitos necessários, mas inexistente, manda-se um erro = true
             throw Error('CEP não existente!')
         }
         var cidade = document.getElementById('cidade');
         var logradouro = document.getElementById('endereco');
         var estado = document.getElementById('estado');
+        var bairro = document.getElementById('bairro')
        
         const cepInput = document.querySelector("#cep")
         cepInput.value = consultaCEPConvertida.cep
         
         cidade.value = consultaCEPConvertida.localidade; //nome do que retorna na api com o valor de cidade
-        logradouro.value = consultaCEPConvertida.logradouro;
+        endereco.value = consultaCEPConvertida.logradouro;
         estado.value = consultaCEPConvertida.uf;
-
+        bairro.value = consultaCEPConvertida.bairro;
 
         console.log(consultaCEPConvertida);
         return consultaCEPConvertida
     }
     catch(erro){//erro 400 por ex
+        //1 mensagemErro.innerHTML = `<p>CEP inválido. Tente novamente!<p>`
         console.log(erro)
     }
  
@@ -29,6 +34,13 @@
 
 //consulta dinâmica
 var cep = document.getElementById("cep");
-cep.addEventListener("focusout", () => buscaEndereco(cep.value))
+cep.addEventListener("focusout", () => limpaInput());
+cep.addEventListener("focusout", () => buscaEndereco(cep.value));
 
-//preenchimento automático
+
+function limpaInput(){
+    cidade.value = ""; 
+    endereco.value = "";
+    estado.value = "";
+    bairro.value = "";
+}
